687452d1cdc0c4be16cbcacc7143465204849350
adan-enigmaticads <adan@enigmaticads.com>
2025-07-22 06:34:42 -0400

    added ad tag

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 
 const app = express();
-const PORT = 5000;
+const PORT = 4000;
 
 // Path to posts.json
 const POSTS_FILE = path.join(__dirname, 'public', 'posts.json');
